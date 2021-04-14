aed24383ea9678e12f34256e5c218de639570b2f
German Brassini <german.brassini@gmail.com>
2021-04-14 20:12:31 -0300

    Estructura estatica previa dinamizacion (hbs)

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('hola mundo!');
+
+const express = require('express')
+const app = express()
+const port = 8080
+
+// Servir contenido estatico
+app.use(express.static('public'));
+
+app.get('/generic', (req, res) => {
+  res.sendFile(__dirname + '/public/generic.html')
+})
+app.get('/elements', (req, res) => {
+  res.sendFile(__dirname + '/public/elements.html')
+})
+app.get('*', (req, res) => {
+  res.send('404 | Not Found')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
